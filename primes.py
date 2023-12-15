dca0cec0c6e64cf694428797cd12858f5aea6d7a
Gunel Farajli <gunel.farajli@kcl.ac.uk>
2023-12-15 15:50:07 +0000

    Fix pytest and run

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes < 1:
+        raise ValueError(" The number must be positive.")
+    val = 2
+    num_list = []
+
+    while len(num_list) < number_of_primes:
+        if is_prime(val):
+            num_list.append(val)
+        val+=1
+    
+    return num_list
+
+def is_prime(value):
+    is_Prime = True
+    new_val = int(value**0.5)
+    if value < 2:
+        is_Prime = False
+    for i in range(2, (new_val +1)):
+        if value % i == 0:
+            is_Prime = False
+    
+    return is_Prime
